cabd7d40480eba16cd57937c96f3813445648e2d
SrDhruv103 <dhruv.patel@swiftrut.com>
2024-12-26 23:58:14 +0530

    Main Commits

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Component/Navbar/navbar'
+import Home from './Component/Home/home'
+import Main from './Component/Main/main'
+import Footer from './Component/Footer/footer'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <Main/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
